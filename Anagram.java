deb140b7276ce1ae3cc8d7e4921fb8d7eb1de140
khushboop14 <p.khushbooparmar@gmail.com>
2024-06-27 23:16:43 +0530

    updated-day4

--- Anagram.java
+++ Anagram.java
@@ -1,2 +1,25 @@
-package Strings;public class Anagram {
+package Strings;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Anagram {
+    public static void main(String[] args) {
+        String str1, str2;
+        Scanner scanner = new Scanner(System.in);
+        str1 = scanner.nextLine();
+        str2 = scanner.nextLine();
+        boolean result =  isAnagram( str1, str2);
+        System.out.println(result);
+    }
+   public static boolean isAnagram(String s1, String s2){
+       s1 = s1.replaceAll("\\s", "").toLowerCase();
+       s2 = s2.replaceAll("\\s", "").toLowerCase();
+       if (s1.length() != s2.length()) {
+           return false;
+       }
+       char[] charAr1 = s1.toCharArray();
+       char[] charAr2 = s2.toCharArray();
+       Arrays.sort(charAr1);
+       Arrays.sort(charAr2);
+       return  Arrays.equals(charAr1, charAr2);
+    }
 }
